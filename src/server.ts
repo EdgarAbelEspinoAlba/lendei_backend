56e81931698b1a7b13103ff23830bd5e9ff731e5
IronMan <eespino@bzpay.com.mx>
2025-05-18 16:29:12 -0600

    prueba de cors

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,22 @@ import { corsConfig } from './config/cors'
 connectDB()
 const app = express()
 
+const allowedOrigins = ['https://lendei.netlify.app', 'http://localhost:5173'];
+
 app.use(cors(corsConfig))
 /** Leer datos de formulario **/ 
 app.use(express.json())
 app.use('/', router)
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('No permitido por CORS'));
+  },
+  credentials: true,
+}));
 
 export default app
